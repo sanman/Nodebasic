9bd97084cc1b1628d9358e42624249e6e251ce1a
sanjiv <learnsanjeev@gmail.com>
2020-07-26 03:07:11 -0400

    Added JS learning for global, local scope, return in a function, assignment, assiging a return value to a variable, boolean datatype

--- learning.js
+++ learning.js
@@ -208,3 +208,115 @@ function ourReusableFunction() {
     console.log("Hello World, I am inside a function");
 }
 ourReusableFunction();
+
+//Passing values to functions as parameters
+function functionAdd(a, b) {
+    console.log("The result of adding two numbers is " + (a+b));
+}
+functionAdd(5, 10)
+
+//Global scope and Functions - visibility of variables, variables defined outside function have global scope while defined inside function are scoped within that particular function
+var myGlobal = 10;
+function fun1() {
+    oopsGlobal = 5;
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oppsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+fun1();
+fun2();
+
+//Global vs Local scope - local scope takes precedence
+var outerWear = "T-Shirt";
+
+function myOutFit() {
+    var outerWear = "Sweater";
+
+    return outerWear;
+}
+
+console.log(myOutFit()); // Here Sweater is printed
+console.log(outerWear);
+
+//return a value from a function with return
+
+function minusSeven(num) {
+    return num - 7;
+}
+
+console.log(minusSeven(10))
+
+function timesFive(num) {
+    return num * 5;
+}
+
+console.log(timesFive(5))
+
+//Understanding Undefined value returned from a function
+var sum = 0;
+function addThree() {
+    sum += 3;
+}
+//console.log(addThree()) //Get Sum is not defined error
+
+function addFive() {
+Sum += 5;
+}
+
+//console.log(addFive()); //get sum is not defined error
+//
+function name(params) {
+    
+}
+
+
+//Assignment with a returned value
+var changed = 0;
+
+function change(num) {
+    return (num + 5)/3;
+}
+changed = change(10);
+console.log(changed);
+
+//Stand in line
+function nextInLine(arr, item) {
+    arr.push(item)
+    return arr.shift();
+}
+var testArr = [1, 2, 3, 4, 5, 6, 7]
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 8));
+console.log("After: " + JSON.stringify(testArr));
+
+
+//Boolean Values - Another datatype
+function welcomeToBooleans(num) {
+    if (num > 10) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+console.log(welcomeToBooleans(5))
+console.log(welcomeToBooleans(11))
+
+// Use conditional logic with Id statements
+var isItTrue = 2==3;
+function ourTrueOrFalse(isIttrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+
+console.log(ourTrueOrFalse(true))
